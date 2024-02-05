bb94f50d344e88446a2b3016289c01edd9e2e533
connertran <conner.tran@bsa.edu.vn>
2024-02-05 18:02:41 +0100

    adding comments and docstrings to app.py

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ responses = []
 
 @app.route('/')
 def show_homepage():
+  """Showing the homepage"""
   question_number = 0
   return render_template('homepage.html', survey_title = surveys.satisfaction_survey.title, survey_instructions = surveys.satisfaction_survey.instructions, question_number=question_number)
 
 @app.route('/answer',methods=["POST"])
 def append_ans_to_res():
+  """Saving the answers to the database"""
   choice = request.form['answer']
   responses.append(choice)
   print(responses)
@@ -27,24 +29,13 @@ def append_ans_to_res():
 
 @app.route('/thank-you')
 def thank_the_user():
+  """The user has completed the survey"""
   return render_template('complete.html')
 
-# @app.route('/questions/<int:num>')
-# def show_user_the_right_question(num):
-  
-#   answers_in_responses = len(responses)
-#   if num <= answers_in_responses:  
-#     return render_template(f'question.html', question_num = num, question = surveys.satisfaction_survey.questions[num].question, choices = surveys.satisfaction_survey.questions[num].choices)
-#   elif len(surveys.satisfaction_survey.questions) == answers_in_responses:
-#         # They've answered all the questions! Thank them.
-#         return redirect("/thank-you")
-#   else:
-#     flash(f"You're trying to access an invalid question {num}")
-#     return render_template('question.html', question_num = answers_in_responses,question = surveys.satisfaction_survey.questions[answers_in_responses].question, choices = surveys.satisfaction_survey.questions[answers_in_responses].choices)
+
 @app.route('/questions/<int:num>')
 def show_user_the_right_question(num):
-    global responses
-
+    """Show the user the right answer"""
     answers_in_responses = len(responses)
     if num <= answers_in_responses and len(surveys.satisfaction_survey.questions) != answers_in_responses:
         return render_template(
@@ -55,8 +46,6 @@ def show_user_the_right_question(num):
         )
     elif len(surveys.satisfaction_survey.questions) == answers_in_responses:
         # They've answered all the questions! Thank them.
-        # reset the survey
-        responses = []  # Reset the global variable
         return redirect("/thank-you")
     else:
         flash(f"You're trying to access an invalid question {num}")
